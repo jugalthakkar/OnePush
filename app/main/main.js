ba417bae6efd2552da9e2d31c603ca04b4a65b6f
jugalthakkar <jugal.thakkar@gmail.com>
2016-09-18 12:47:37 +0530

    init: angular-seed (- karma - protractor) + bootstrap-material-design

---
+++ app/main/main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('onePush.main', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/', {
+    templateUrl: 'main/main.html',
+    controller: 'MainCtrl'
+  });
+}])
+
+.controller('MainCtrl', [function() {
+
+}]);
